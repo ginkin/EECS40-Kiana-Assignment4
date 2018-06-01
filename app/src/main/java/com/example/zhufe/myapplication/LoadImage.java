a41c8163a75041b43ecbfa5898a76f80fece46ea
sunh7 <sunh7@uci.edu>
2018-06-01 12:10:23 -0700

    load3

--- app/src/main/java/com/example/zhufe/myapplication/LoadImage.java
+++ app/src/main/java/com/example/zhufe/myapplication/LoadImage.java
@@ -7,18 +7,71 @@ import android.graphics.BitmapFactory;
 import android.graphics.Bitmap;
 import android.content.res.Resources;
 
+import game.image.Image;
+
 
 public class LoadImage {
     public static ArrayList<Bitmap> mario = new ArrayList<Bitmap>();
 
-    public static void Loading(Context context){
-        try{
-            for (int i = 1;i<=13;i++){
-                mario.add(BitmapFactory.decodeStream(context.getAssets().open("mario/mario"+i+".png")));
+    public static ArrayList<Bitmap> enemy = new ArrayList<Bitmap>();
+
+    public static ArrayList<Bitmap> coin = new ArrayList<Bitmap>();
+
+    public static ArrayList<Bitmap> blast = new ArrayList<Bitmap>();
+
+    public static ArrayList<Bitmap> food = new ArrayList<Bitmap>();
+
+    public static ArrayList<Bitmap> map = new ArrayList<Bitmap>();
+
+    public static ArrayList<Bitmap> tile = new ArrayList<Bitmap>();
+
+    public static ArrayList<Bitmap> weapon = new ArrayList<Bitmap>();
+
+    public static ArrayList<Bitmap> ui = new ArrayList<Bitmap>();
+
+
+    public static void LoadImage(Context context) {
+        try {
+            for (int i = 1; i <= 13; i++) {
+                mario.add(BitmapFactory.decodeStream(context.getAssets().open("mario/mario" + i + ".png")));
             }
-        }catch (IOException e){
+
+            for (int i = 1; i <= 12; i++) {
+                enemy.add(BitmapFactory.decodeStream(context.getAssets().open("enemy/enemy" + i + ".png")));
+            }
+
+            for (int i = 1; i <= 4; i++) {
+                coin.add(BitmapFactory.decodeStream(context.getAssets().open("coin/coin" + i + ".png")));
+            }
+
+            for (int i = 1; i <= 3; i++) {
+                blast.add(BitmapFactory.decodeStream(context.getAssets().open("blast/blast" + i + ".png")));
+            }
+
+            for (int i = 1; i <= 3; i++) {
+                food.add(BitmapFactory.decodeStream(context.getAssets().open("food/food" + i + ".png")));
+            }
+
+            for (int i = 1; i <= 4; i++) {
+                Bitmap m = BitmapFactory.decodeStream(context.getAssets().open("map/map" + i + ".jpg"));
+
+                m = Image.FitTheScreenSizeImage(m, Load.ScreenWidth, Load.ScreenHeight);
+                map.add(m);
+            }
+
+            for (int i = 1; i <= 35; i++) {
+                tile.add(BitmapFactory.decodeStream(context.getAssets().open("tile/tile" + i + ".png")));
+            }
+
+            for (int i = 1; i <= 2; i++) {
+                weapon.add(BitmapFactory.decodeStream(context.getAssets().open("weapon/weapon" + i + ".png")));
+            }
+
+            for (int i = 1; i <= 2; i++) {
+                ui.add(BitmapFactory.decodeStream(context.getAssets().open("ui/ui" + i + ".png")));
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
